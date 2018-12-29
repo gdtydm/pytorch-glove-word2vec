7633c57f0114bdeb42df1ff0b926a9d48199487b
liqing <liqing@qffenqigo.com>
2018-12-29 17:13:47 +0800

    add skip-gram

--- word2vec.py
+++ word2vec.py
@@ -2,6 +2,8 @@ from tools import CorpusPreprocess, VectorEvaluation
 from skip_gram import SkipGram
 import torch
 from functools import partial
+from tqdm import tqdm
+
 
 class Word2Vec():
     def __init__(self,input_file_path, output_vector_path, neg_num=10, epoches = 10,bach_size = 5,windows_size=5, embedding_dim=300, min_count=5, model_class="skip-gram", optimizer_mothod="huffman"):
@@ -19,7 +21,6 @@ class Word2Vec():
         self.optimizer_mothod = optimizer_mothod
         self.use_cuda = torch.cuda.is_available()
         self.build_model()
-    
 
     
     def build_model(self):
@@ -71,8 +72,36 @@ class Word2Vec():
                 optimizer.step()
                 if steps % 100 == 0:
                     print(f"Epoch {epoch} steps {steps}, loss {loss.item()/len(batch)}")
+                    
                 steps += 1
+        self.save_vector()
+        self.evaluation_vector(f"./data/{self.model_class}_{self.optimizer_mothod}.png",
+        word="中国", w_num=10)
+    
+    def save_vector(self):
+        if hasattr(self.model, "embedding_matrix"):
+            metrix = self.model.embedding_matrix.weight.data
+        else:
+            metrix = self.model.v_embedding_matrix.weight.data
+        with open(self.output_vector_path, "w", encoding="utf-8") as f:
+            if self.use_cuda:
+                vector= metrix.cpu().numpy()
+            else:
+                vector = metrix.numpy()
+            for i in tqdm(range(len(vector))):
+                word = self.data_processor.idex2word[i]
+                s_vec = vector[i]
+                s_vec = [str(s) for s in s_vec.tolist()]
+                write_line = word + " " + " ".join(s_vec)+"\n"
+                f.write(write_line)
+            print("Word2vec vector save complete!")
 
+    def evaluation_vector(self, picture_path, word, w_num):
+        ve = VectorEvaluation(self.output_vector_path)
+        ve.drawing_and_save_picture(picture_path)
+        ve.get_similar_words(word, w_num)
+
+        
 if __name__ == "__main__":
-    w2v = Word2Vec(input_file_path="./data/zhihu.txt",output_vector_path="./data/skip-gram.vec")
+    w2v = Word2Vec(input_file_path="./data/zhihu.txt",output_vector_path="./data/skip-gram.vec", optimizer_mothod="huffman")
     w2v.train_model()
